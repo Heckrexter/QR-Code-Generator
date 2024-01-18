f8f1358226ee6f9d8b425814298f70560224c53c
Heckrexter <102521099+Heckrexter@users.noreply.github.com>
2024-01-18 23:26:12 +0530

    remove useless code from script.js

--- script.js
+++ script.js
@@ -4,14 +4,12 @@ const image = document.getElementById("qrpic");
 let maina = document.getElementsByClassName("main")[0];
 
 function generate() {
-    // set propery to custom state "expanded" in css
-    // maina.style.setState({ expand: true });
-    maina.classList.toggle('showbord');
     let test = document.getElementById("dinput").value;
     if (test == "") {
         image.src = "/api.qrserver.png";
         return;
     }
     let finalurl = `${baseurl}?data=${test}&bg-color=[240-248-255]`;
+    console.log(finalurl);
     image.src = finalurl;
 }
